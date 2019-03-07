3d0de024f3f142e5f2913563aa4fd48737d71670
Mustafa Aga <mustafaaga23@gmail.com>
2019-03-07 22:13:49 +0100

    subgroup

--- ShopApp/SubGroupControl.xaml.cs
+++ ShopApp/SubGroupControl.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,12 +31,45 @@ namespace ShopApp
         {
 
             // Do not load your data at design time.
-            // if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
-            // {
-            // 	//Load your data here and assign the result to the CollectionViewSource.
-            // 	System.Windows.Data.CollectionViewSource myCollectionViewSource = (System.Windows.Data.CollectionViewSource)this.Resources["Resource Key for CollectionViewSource"];
-            // 	myCollectionViewSource.Source = your data
-            // }
+            if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
+            {
+                ShopApp.InventoryDataSet inventoryDataSet = ((ShopApp.InventoryDataSet)(this.FindResource("inventoryDataSet")));
+                // Load data into the table Reparts. You can modify this code as needed.
+                ShopApp.InventoryDataSetTableAdapters.GroupsTableAdapter inventoryDataSetRepartsTableAdapter = new ShopApp.InventoryDataSetTableAdapters.GroupsTableAdapter();
+                inventoryDataSetRepartsTableAdapter.Fill(inventoryDataSet.Groups);
+                System.Windows.Data.CollectionViewSource repartsViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("groupsViewSource")));
+                repartsViewSource.View.MoveCurrentToFirst();
+                LoadData();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddSubGroup subGroup = new AddSubGroup(this);
+            subGroup.Show();
+        }
+
+        private void CmbxGroupId_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LoadData();
+        }
+
+        public void LoadData(string groupId = "0")
+        {
+
+             groupId = groupId=="0"? cmbxGroupId.SelectedValue.ToString() : groupId;
+            string query = String.Format(@"select SubGroup_ID, SubGroup_Name,SubGroup_Desc,s.Date_Created, u.Username as Created_By  from SubGroups s
+            inner join users u on u.User_ID = s.Created_By  where s.Group_ID = {0}", groupId);
+
+
+            using (SqlConnection con = new SqlConnection(Utility.ConnStr))
+            {
+                SqlCommand cmd = new SqlCommand(query, con);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable("SubGroups");
+                sda.Fill(dt);
+                subGroupsDataGrid.ItemsSource = dt.DefaultView;
+            }
         }
     }
 }
