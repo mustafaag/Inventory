1f7399075cca5987c0db0284ba4e08e20e26e758
mustafaag <36820880+mustafaag@users.noreply.github.com>
2019-03-12 03:14:56 +0100

    Transferimet finished

--- ShopApp/MovesControl.xaml.cs
+++ ShopApp/MovesControl.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +26,8 @@ namespace ShopApp
         {
             InitializeComponent();
         }
-
+        DataSet ds;
+        SqlDataAdapter adapter;
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
 
@@ -50,7 +53,104 @@ namespace ShopApp
          
         public void LoadData()
         {
+            this.ds = new DataSet();
+            try
+            {
+                string data = dtpxdateSelected.SelectedDate.ToString();
+                string query = string.Format(@"select 
+                                            t.ID,
+                                            t.DateCreated,
+                                            r.Repart_Name as RepartTo,
+                                            r2.Repart_Name as RepartFrom,
+                                            d.Depot_Name as MagFrom,
+                                            d1.Depot_Name as MagTo,
+                                            p.Product_Name, 
+                                            t.Quantity,
+                                            t.CostPerUnit,
+                                            t.ProductMoveCost,
+                                            t.Comments,
+                                            t.ProductCost,
+                                            n.Njesia_Name
+                                            from Transactions t 
+                                            inner join Reparts r on r.Repart_Id = t.RepIdTo
+                                            inner join Reparts r2 on r2.Repart_Id = t.RepIdFrom
+                                            inner join Depots d on d.Depot_ID = t.DepotIdFrom
+                                            inner join Depots d1 on d1.Depot_ID = t.DepotIdTo
+                                            inner join Products p on p.Product_ID = t.ProductId
+                                            inner join Njesite n on n.Njesia_ID =  t.NjesiaId
+                                            where cast(DateCreated as date) = cast('{0}' as date)
+                                            order by t.ID desc ", data);
+
+
+                using (SqlConnection con = new SqlConnection(Utility.ConnStr))
+                {
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable("Transactions");
+                    sda.Fill(dt);
+                    transactionsDataGrid.ItemsSource = dt.DefaultView;
+                    adapter = sda;
+                }
+                this.ds.AcceptChanges();
+            }
+            catch (Exception ec)
+            {
+
+                throw ec;
+            }
+            
 
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.ds = new DataSet();
+            try
+            {
+                string data = txtTransferId.Text;
+                string query = string.Format(@"select 
+                                            t.ID,
+                                            t.DateCreated,
+                                            r.Repart_Name as RepartTo,
+                                            r2.Repart_Name as RepartFrom,
+                                            d.Depot_Name as MagFrom,
+                                            d1.Depot_Name as MagTo,
+                                            p.Product_Name, 
+                                            t.Quantity,
+                                            t.CostPerUnit,
+                                            t.ProductMoveCost,
+                                            t.Comments,
+                                            t.ProductCost,
+                                            n.Njesia_Name
+                                            from Transactions t 
+                                            inner join Reparts r on r.Repart_Id = t.RepIdTo
+                                            inner join Reparts r2 on r2.Repart_Id = t.RepIdFrom
+                                            inner join Depots d on d.Depot_ID = t.DepotIdFrom
+                                            inner join Depots d1 on d1.Depot_ID = t.DepotIdTo
+                                            inner join Products p on p.Product_ID = t.ProductId
+                                            inner join Njesite n on n.Njesia_ID =  t.NjesiaId
+                                            where t.id={0}
+                                            ", data);
+
+
+                using (SqlConnection con = new SqlConnection(Utility.ConnStr))
+                {
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable("Transactions");
+                    sda.Fill(dt);
+                    adapter = sda;
+                    transactionsDataGrid.ItemsSource = dt.DefaultView;
+                }
+                this.ds.AcceptChanges();
+            }
+            catch (Exception ec)
+            {
+
+                throw ec;
+            }
+
+        }
+       
     }
 }
