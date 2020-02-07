0ff799c58b11b18eae4fc4d1a812c9ffcd9dcf5d
mustafaag <mustafaaga23@gmail.com>
2020-02-07 16:10:39 +0100

    first commit

--- ShopApp/RecepturaControl.xaml.cs
+++ ShopApp/RecepturaControl.xaml.cs
@@ -27,6 +27,17 @@ namespace ShopApp
                 inventoryDataSetRepartsTableAdapter.Fill(inventoryDataSet.Reparts);
                 System.Windows.Data.CollectionViewSource repartsViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("repartsViewSource")));
                 repartsViewSource.View.MoveCurrentToFirst();
+
+
+                ShopApp.InventoryDataSetTableAdapters.SubGroupsTableAdapter inventoryDataSetRepartsTableAdapter1 = new ShopApp.InventoryDataSetTableAdapters.SubGroupsTableAdapter();
+                inventoryDataSetRepartsTableAdapter1.Fill(inventoryDataSet.SubGroups);
+                System.Windows.Data.CollectionViewSource repartsViewSource1 = ((System.Windows.Data.CollectionViewSource)(this.FindResource("subGroupsViewSource")));
+                repartsViewSource1.View.MoveCurrentToFirst();
+
+                ShopApp.InventoryDataSetTableAdapters.ProductsTableAdapter inventoryDataSetRepartsTableAdapter2 = new ShopApp.InventoryDataSetTableAdapters.ProductsTableAdapter();
+                inventoryDataSetRepartsTableAdapter2.Fill(inventoryDataSet.Products);
+                System.Windows.Data.CollectionViewSource repartsViewSource2 = ((System.Windows.Data.CollectionViewSource)(this.FindResource("productsViewSource")));
+                repartsViewSource2.View.MoveCurrentToFirst();
             }
         }
 
@@ -44,23 +55,14 @@ namespace ShopApp
                 DataTable dt = new DataTable("SubGroups");
                 sda.Fill(dt);
                 cmbSubGroupId.ItemsSource = dt.DefaultView;
+   
+                cmbSubGroupId.SelectedValuePath = "SubGroup_Id";
             }
         }
 
         private void CmbSubGroupId_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-           
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            AddRecepture addRecepture = new AddRecepture();
-            addRecepture.Show();
-        }
-
-        private void CmbSubGroupId_DropDownClosed(object sender, System.EventArgs e)
-        {
-           
+            if (cmbSubGroupId.SelectedValue == null) return;
             string subgroupId = cmbSubGroupId.SelectedValue.ToString();
             string query = string.Format(@"Select Product_ID,Product_Name from Products where Sub_Group_ID={0}", subgroupId);
             using (SqlConnection con = new SqlConnection(Utility.ConnStr))
@@ -71,7 +73,39 @@ namespace ShopApp
                 sda.Fill(dt);
                 cmbxProductMaster.ItemsSource = dt.DefaultView;
             }
-
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddRecepture addRecepture = new AddRecepture();
+            addRecepture.Show();
+        }
+
+        private void CmbxProductMaster_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            string masterId = cmbxProductMaster.SelectedValue.ToString();
+            string query = string.Format(@"Select r.ID, p.Product_Name as masterProduct , pr.Product_Name as additional, r.Quantity, r.CostPerUnit,r.CostTotal, nj.Njesia_Name as Njesia, r.Comments
+                                         from Recepturat r
+                                         inner join Products p on p.Product_ID = r.MasterProductId
+                                         inner join Products pr on pr.Product_ID = r.AdditionalProduct 
+                                         inner join Njesite nj on nj.Njesia_ID = r.Measure_Id
+                                         where MasterProductId = {0}", masterId);
+            using (SqlConnection con = new SqlConnection(Utility.ConnStr))
+            {
+                SqlCommand cmd = new SqlCommand(query, con);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable("Recepturat");
+                sda.Fill(dt);
+
+                recepturatDataGrid.ItemsSource = dt.DefaultView;
+            }
+        }
+
+    
     }
 }
